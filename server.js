7632fa6ed40d6d75663b0b8c2b705ae18b684d40
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-11 09:51:17 -0400

    add explanation

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// I tried serving the files from '/' and found out that you don't have access to __dirname in ESM modules since they're supposed to work cross-platform and __dirname doesn't exist in the browser. So you have to create it like this:
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
@@ -21,9 +22,11 @@ app.get("/video", (req, res) => {
   const CHUNK_SIZE = 10 ** 6; // 1MB
   // request will show up with Range as "bytes=232323-" with the dash meaning the rest of the bytes after that point. Here we take out all non-digit characters to convert it to a number.
   const start = Number(range.replace(/\D/g, ""));
-  // add the chunk size to the start to find the end, or grab the end of the video
+  // add the chunk size to the start to find the end, or grab the end of the video. "-1 included because byte byte positions are 0-based i.e. for 500 bytes the last byte is 499"
   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  // same thing with +1
   const contentLength = end - start + 1;
+  // ok I get it now - so regardless of whether we handle it here, the browser is going to send a range request for a video starting with 0- (ie. "give me the whole video"). The server responds with a stream of a specific chunk, after which the client will send another request starting at the end of the last chunk.
   const headers = {
     "Content-Range": `bytes ${start}-${end}/${videoSize}`,
     "Accept-Ranges": "bytes",
